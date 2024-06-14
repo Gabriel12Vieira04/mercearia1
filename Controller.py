f5845cafb86c1720a5b822c2d195a944d09dae27
Gabriel12Vieira04 <bielsobrenatural99@gmail.com>
2024-06-13 22:13:27 -0400

    commit3

--- Controller.py
+++ Controller.py
@@ -31,7 +31,7 @@ class ControllerCategoria:
                     del x[i]
                     break
             print('Categoria removida com sucesso')
-
+        #TODO: COLOCAR SEM CATEGORIA NO ESTOQUE
             with open('categoria.txt',  'w') as arq:
                 for i in x:
                     arq.writelines(i.categoria)
@@ -48,6 +48,7 @@ class ControllerCategoria:
             if len(cat1) == 0:
                 x = list(map(lambda x: Categoria(categoriaAterada)if(x.categoria == categoriaAlterar) else(x), x))
                 print("Categoria alterada com sucesso")
+                #TODO: ALTERAR A CATEGORIA TAMBEM DO ESTOQUE
             else:
                 print("A categoria para qual deseja alterar já existente")
 
@@ -59,6 +60,30 @@ class ControllerCategoria:
                 arq.writelines(i.categoria)
                 arq.writelines('\n')
 
+    def mostra_Categoria(self):
+        categoria = DaoCategoria.ler()
+        if len(categoria) == 0:
+            print('Categoria Vazia')
+        else:
+            for i in categoria:
+                print(f'Categoria {i.categoria}')
 
-a = ControllerCategoria()
-a.alterarCAtegoria("CArnes", "Verduras")
+class ControllerEstoque:
+    def cadastraProduto(self, nome, preco, categoria, quantidade):
+        x = DaoEstoque.ler()
+        y = DaoCategoria.ler()
+        h = list(filter(lambda x: x.categoria == categoria, y))
+        est = list(filter(lambda x: x.produto.nome == nome, x))
+
+        if len(h) > 0:
+            if len(est) == 0:
+                produto = Produtos(nome, preco, categoria)
+                DaoEstoque.salvar(produto, quantidade)
+                print('Produto cadastrada com sucesso')
+            else:
+                print('Produto já exite em estoque')
+        else:
+            print('Categoria não existe')
+
+a = ControllerEstoque()
+a.cadastraProduto('tomate', '5', 'Verduras', 10)
